c1508ee2b59cc499e3fcc02ad200dd16d1e357a6
Merouane Atig <yahoocbien-dev@yahoo.fr>
2013-06-14 16:35:21 +0200

    first passing test for PostController

---
+++ MVCBlog.Tests/Controllers/PostControllerTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace MVCBlog.Tests.Controllers
+{
+    [TestClass]
+    public class PostControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            Mock<IPostRepository> postRepository = new Mock<IPostRepository>();
+
+            new PostController(postRepository.Object).Index();
+
+
+            postRepository.Verify(x => x.GetAllPosts());
+        }
+    }
+
+    public class PostController : Controller
+    {
+        private readonly IPostRepository _postRepository;
+
+        public PostController(IPostRepository postRepository)
+        {
+            _postRepository = postRepository;
+        }
+
+        public void Index()
+        {
+            _postRepository.GetAllPosts();
+        }
+    }
+
+    public interface IPostRepository
+    {
+        IEnumerable<Post> GetAllPosts();
+    }
+
+    public class Post
+    {
+    }
+}
